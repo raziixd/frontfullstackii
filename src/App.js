b44bed3b8be8149b8d6f580a480e8c7fa974354f
Gabriel Carvalho <zerowings_@hotmail.com>
2023-08-25 09:06:19 -0300

    funciona

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import TelaCadastro from "./telas/TelasCadastro";
+import TelaMenu from "./telas/TelaMenu";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Tela404 from "./telas/tela404";
+
+function App() {
+  return (
+    <div>
+     <BrowserRouter>
+      <Routes>
+          <Route path="/FormUsuario" element={<TelaCadastro/>}/>          
+          <Route path="/" element={<TelaMenu/>}/>
+          <Route path="*" element={<Tela404/>}/>
+      </Routes>
+     </BrowserRouter>     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utilitarios/definicoes.js
@@ -0,0 +1,3 @@
+export const urlBase = "http://localhost:3005";
+export const urlBase2 = "http://localhost:3006";
+export const urlBase3 = "https://129.146.68.51/aluno19-pfsii";
