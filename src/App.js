27433ef0748e610f6596f78742df4fdc8b89701d
Gabriel Carvalho <zerowings_@hotmail.com>
2023-09-22 10:15:43 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TelaMenu from "./telas/TelaMenu";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Tela404 from "./telas/tela404";
 import TelaDoacao from "./telas/TelaDoacao";
-import Home from "./home";
+// import Home from "./home";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/FormUsuario" element={<TelaCadastro />} />
           <Route path="/FormDoacao" element={<TelaDoacao />} />
-          <Route path="/home" element={<Home />} />
+          {/* <Route path="/home" element={<Home />} /> */}
           <Route path="/" element={<TelaMenu />} />
           <Route path="*" element={<Tela404 />} />
         </Routes>
