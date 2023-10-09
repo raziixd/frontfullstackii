bdbe3fed3e28369e63e80f46c24621ec3c7ea929
Gabriel Carvalho <zerowings_@hotmail.com>
2023-10-09 15:04:37 -0300

    oremos

--- src/utilitarios/definicoes.js
+++ src/utilitarios/definicoes.js
@@ -1,6 +1,6 @@
-// export const urlBase = "http://localhost:3007/material";
-export const urlBase = "https://129.146.68.51/aluno19-pfsii/material";
-// export const urlBase2 = "http://localhost:3007/doacao";
-export const urlBase2 = "https://129.146.68.51/aluno19-pfsii/doacao";
-// export const urlBase3 = "http://localhost:3007/usuarios";
-export const urlBase3 = "https://129.146.68.51/aluno19-pfsii/usuarios";
+export const urlBase = "http://localhost:3007/material";
+// export const urlBase = "https://129.146.68.51/aluno19-pfsii/material";
+export const urlBase2 = "http://localhost:3007/doacao";
+// export const urlBase2 = "https://129.146.68.51/aluno19-pfsii/doacao";
+export const urlBase3 = "http://localhost:3007/usuarios";
+// export const urlBase3 = "https://129.146.68.51/aluno19-pfsii/usuarios";
